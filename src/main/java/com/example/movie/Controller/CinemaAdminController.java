f37cea5f9ef0aa1c13af01d8bdcdcc96dca8cd1e
wsy12138 <1181213656@qq.com>
2020-03-21 14:31:46 +0800

    web cinema-admin movie model

--- src/main/java/com/example/movie/Controller/CinemaAdminController.java
+++ src/main/java/com/example/movie/Controller/CinemaAdminController.java
@@ -7,12 +7,16 @@ import com.example.movie.Entity.Movie;
 import com.example.movie.Entity.User;
 import com.example.movie.Service.Cinema_AdminService;
 
+import com.example.movie.Service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -20,6 +24,8 @@ import java.util.List;
 public class CinemaAdminController {
     @Autowired
     private Cinema_AdminService cinema_adminService;
+    @Autowired
+    private MovieService movieService;
 
     @PostMapping(path="/login")
     public String login(@RequestParam("username") String username,
@@ -33,7 +39,6 @@ public class CinemaAdminController {
         }
 
     }
-
     @PostMapping(path = "/add")
     public String add(@RequestParam String username, String password)
     {
@@ -41,12 +46,38 @@ public class CinemaAdminController {
         return "redirect:/login";
     }
 
+    @GetMapping(path = "/allMovies")
+    public String displayAllMovies(Model model)
+    {
+        List<Movie> movies=movieService.display_all_movies();
+        model.addAttribute("movies",movies);
+        return "manage_movie";
+    }
 
-//    @GetMapping(path="/movies_in_cinema")
-//    public @ResponseBody String moviesInCinema(@CurrentUser Cinema_Admin cinema_admin, Model model) {
-//        List<Movie> movies=new ArrayList<>();
-//        movies=cinema_adminService.findMovieByCinema(cinema_admin.getCinema().getId());
-//        model.addAttribute("movies",movies);
-//        return "manage_movie";
-//    }
+    @GetMapping(path = "/addMovieForm")
+    public String displayMovieForm()
+    {
+        return "movie_form";
+    }
+
+    @PostMapping(path = "/addMovies")
+    public String addMovies(@RequestParam String title, String blurb, String certificate, String director, String actors,
+                            String showtime, Integer duration, String type, String language, String url)
+    {
+        try {
+            SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
+            movieService.addMovies(title,blurb,certificate,director,actors,
+                    sdf.parse(showtime),duration,type,language,url);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return "redirect:/cinemaAdmin/allMovies";
+    }
+
+
+
+
+
+
 }
