f891c479587b55cf86e49b285b681d28898f0e0c
978633302 <978633302@qq.com>
2020-03-31 22:21:09 +0800

    admin_login

--- src/main/java/com/example/movie/Repository/ScreeningRepository.java
+++ src/main/java/com/example/movie/Repository/ScreeningRepository.java
@@ -15,6 +15,10 @@ public interface ScreeningRepository extends CrudRepository<Screening,Integer> {
     @Query(value = "select * from screening where screen_id = ?",nativeQuery = true)
     public List<Screening> find_screenings_by_screen_id(Integer id);
 
+    @Query(value = "select * from screening where id = ?", nativeQuery = true)
+    public List<Screening> find_screening_by_screening_id(Integer id);
+
+
     @Modifying //may delete
     @Query(value = "delete from screening where movie_id = ?",nativeQuery = true)  //may delete
     public void deleteBymovieID(Integer id);  //may delete
